5a3fe3f806199e3a25acea8c79037bc52fb5b7a1
jeremyletang <letang.jeremy@gmail.com>
2020-10-03 08:38:28 +0100

    add pprof profiling

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var args struct {
@@ -17,6 +20,7 @@ var args struct {
 	blockchainDelay int
 	blockchainRnd   int
 	runtime         int
+	withPPROF       string
 }
 
 func init() {
@@ -30,6 +34,7 @@ func init() {
 	flag.IntVar(&args.blockchainDelay, "blockchainDelay", 500, "block processing time")
 	flag.IntVar(&args.blockchainRnd, "blockchainRnd", 0, "random part thereof")
 	flag.IntVar(&args.runtime, "runtime", 0, "number of ticks before the simulation stops, run forever by default")
+	flag.StringVar(&args.withPPROF, "withPPROF", "", "a string containing a directory path in which to store the CPU/MEM profile. profiling is not started if empty")
 }
 
 func main() {
@@ -40,19 +45,60 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Wendy starting")
-	initWendy()
-	r = args.r
-	n = args.n
-	t = args.t
-	debugLeader = args.debugLeader
-	delay = args.delay
-	msgDelay = args.msgDelay
-	msgRnd = args.msgRnd
-	blockchainDelay = args.blockchainDelay
-	blockchainRnd = args.blockchainRnd
-	runtime = args.runtime
-	//messagebuffer={};
-	sendMessageWithTime("NIL", "BlockTrigger", 0, 0, worldTime+101)
-	networkNew()
+	var (
+		pprof *pprofh
+		err   error
+	)
+	if len(args.withPPROF) > 0 {
+		// a path is specified, we try start profiling
+		pprof, err = newPPROF(args.withPPROF)
+		if err != nil {
+			fmt.Printf("error: unable to start pprof profiling (%v)", err)
+			os.Exit(1)
+		}
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// we start the main wendy routine
+	go func() {
+		fmt.Println("Wendy starting")
+		initWendy()
+		r = args.r
+		n = args.n
+		t = args.t
+		debugLeader = args.debugLeader
+		delay = args.delay
+		msgDelay = args.msgDelay
+		msgRnd = args.msgRnd
+		blockchainDelay = args.blockchainDelay
+		blockchainRnd = args.blockchainRnd
+		runtime = args.runtime
+		//messagebuffer={};
+		sendMessageWithTime("NIL", "BlockTrigger", 0, 0, worldTime+101)
+		networkNew()
+		// we are done here, let's cancel the context
+		cancel()
+	}()
+
+	// catch user signals
+	var ch = make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM)
+	signal.Notify(ch, syscall.SIGINT)
+
+	// wait for a signal, or wendy to finish
+	select {
+	case <-ch:
+		fmt.Printf("wendy simulation cancelled by user request\n")
+	case <-ctx.Done():
+		fmt.Printf("wendy simulation done\n")
+	}
+
+	// if pprof was enabled we stop it
+	if pprof != nil {
+		if err := pprof.Stop(); err != nil {
+			fmt.Printf("error: could not stop pprof profiling properly (%v)", err)
+			os.Exit(1)
+		}
+	}
 }
