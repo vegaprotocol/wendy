8eedc813b84f8fb7327c4f96fd87db5d10e75030
Gustavo Chain <gchain@pm.me>
2021-05-05 18:06:21 +0200

    core: implement recompute

--- core/core_test.go
+++ core/core_test.go
@@ -37,6 +37,7 @@ var (
 	tx2 = newTestTxStr("tx2", "h2")
 	tx3 = newTestTxStr("tx3", "h3")
 	tx4 = newTestTxStr("tx4", "h4")
+	tx5 = newTestTxStr("tx5", "h5")
 )
 
 func TestIsBlockedBy(t *testing.T) {
@@ -104,51 +105,111 @@ func TestIsBlocked(t *testing.T) {
 	})
 }
 
-func TestFairnessLoop(t *testing.T) {
-	// For block order fairness, the most critical test case is a fairness
-	// loop. Say we have four validators and four transactions, and the order
-	// is:
-	//         +-----------------------+
-	//         |       Sequence        |
-	// +-------+-----+-----+-----+-----+
-	// | Node  |  0  |  1  |  2  |  3  |
-	// +-------+-----+-----+-----+-----+
-	// | Node0 | Tx1 | Tx2 | Tx3 | Tx4 |
-	// | Node1 | Tx2 | Tx3 | Tx4 | Tx1 |
-	// | Node2 | Tx3 | Tx4 | Tx1 | Tx2 |
-	// | Node3 | Tx4 | Tx1 | Tx2 | Tx3 |
-	// +-------+-----+-----+-----+-----+
-	//
-	// In this case, we have a loop, that tx1 has priority over tx2, which has
-	// priority over tx3, which has priority over tx4, which has priority over
-	// tx1. This is, in fact, the reason we can only require that transactions
-	// end up in the same block, rather than implementing an order right away.
-
+func newWendyFromTxsMap(txsMap map[ID][]Tx) *Wendy {
 	w := New()
-	w.UpdateValidatorSet([]Validator{
-		"Node0", "Node1", "Node2", "Node3",
-	})
 
-	txsMap := map[ID][]Tx{
-		"Node0": {tx1, tx2, tx3, tx4},
-		"Node1": {tx2, tx3, tx4, tx1},
-		"Node2": {tx3, tx4, tx1, tx2},
-		"Node3": {tx4, tx1, tx2, tx3},
+	var nodes []Validator
+	for node := range txsMap {
+		nodes = append(nodes, Validator(node))
 	}
+	w.UpdateValidatorSet(nodes)
 
 	for node, txs := range txsMap {
 		// we use the tx index as seq
 		for i, tx := range txs {
 			seq := uint64(i)
-			t.Logf("%s: AddVote(seq=%d, tx=%s)", node, seq, tx)
 			w.AddVote(newVote(node, seq, tx))
+			w.AddTx(tx)
 		}
-		t.Logf("")
 	}
 
-	// If tx2 IsBlockedBy tx1 we say that tx1 has priority over tx2.
-	require.True(t, w.IsBlockedBy(tx2, tx1))
-	require.True(t, w.IsBlockedBy(tx3, tx2))
-	require.True(t, w.IsBlockedBy(tx4, tx3))
-	require.True(t, w.IsBlockedBy(tx1, tx4))
+	return w
+}
+
+func TestBlockingSet(t *testing.T) {
+	t.Run("FairnessLoop", func(t *testing.T) {
+		// For block order fairness, the most critical test case is a fairness
+		// loop. Say we have four validators and four transactions, and the order
+		// is:
+		//         +-----------------------------+
+		//         |       Sequence              |
+		// +-------+-----+-----+-----+-----+-----+
+		// | Node  |  0  |  1  |  2  |  3  |  4  |
+		// +-------+-----+-----+-----+-----+-----+
+		// | Node0 | Tx1 | Tx2 | Tx3 | Tx4 | Tx5 |
+		// | Node1 | Tx2 | Tx3 | Tx4 | Tx5 | Tx1 |
+		// | Node2 | Tx3 | Tx4 | Tx5 | Tx1 | Tx2 |
+		// | Node3 | Tx4 | Tx5 | Tx1 | Tx2 | Tx3 |
+		// | Node4 | Tx5 | Tx1 | Tx2 | Tx3 | Tx4 |
+		// +-------+-----+-----+-----+-----+-----+
+		//
+		// In this case, we have a loop, that tx1 has priority over tx2, which has
+		// priority over tx3, which has priority over tx4, which has priority over
+		// tx1. This is, in fact, the reason we can only require that transactions
+		// end up in the same block, rather than implementing an order right away.
+
+		w := newWendyFromTxsMap(
+			map[ID][]Tx{
+				"Node0": {tx1, tx2, tx3, tx4, tx5},
+				"Node1": {tx2, tx3, tx4, tx5, tx1},
+				"Node2": {tx3, tx4, tx5, tx1, tx2},
+				"Node3": {tx4, tx5, tx1, tx2, tx3},
+				"Node4": {tx5, tx1, tx2, tx3, tx4},
+			},
+		)
+
+		// If tx2 IsBlockedBy tx1 we say that tx1 has priority over tx2.
+		require.True(t, w.IsBlockedBy(tx2, tx1))
+		require.True(t, w.IsBlockedBy(tx3, tx2))
+		require.True(t, w.IsBlockedBy(tx4, tx3))
+		require.True(t, w.IsBlockedBy(tx1, tx4))
+
+		set := w.BlockingSet()
+
+		// all txs depends on all txs, hence a loop exists
+		allTxs := []Tx{tx1, tx2, tx3, tx4, tx5}
+		for _, tx := range allTxs {
+			assert.ElementsMatch(t, set[tx.Hash()], allTxs)
+		}
+	})
+
+	t.Run("FullyAgree", func(t *testing.T) {
+		// For block order fairness, the most critical test case is a fairness
+		// loop. Say we have four validators and four transactions, and the order
+		// is:
+		//         +-----------------------------+
+		//         |       Sequence              |
+		// +-------+-----+-----+-----+-----+-----+
+		// | Node  |  0  |  1  |  2  |  3  |  4  |
+		// +-------+-----+-----+-----+-----+-----+
+		// | Node0 | Tx1 | Tx2 | Tx3 | Tx4 | Tx5 |
+		// | Node1 | Tx2 | Tx3 | Tx4 | Tx5 | Tx1 |
+		// | Node2 | Tx3 | Tx4 | Tx5 | Tx1 | Tx2 |
+		// | Node3 | Tx4 | Tx5 | Tx1 | Tx2 | Tx3 |
+		// | Node4 | Tx5 | Tx1 | Tx2 | Tx3 | Tx4 |
+		// +-------+-----+-----+-----+-----+-----+
+		//
+		// In this case, we have a loop, that tx1 has priority over tx2, which has
+		// priority over tx3, which has priority over tx4, which has priority over
+		// tx1. This is, in fact, the reason we can only require that transactions
+		// end up in the same block, rather than implementing an order right away.
+
+		w := newWendyFromTxsMap(
+			map[ID][]Tx{
+				"Node0": {tx1, tx2, tx3, tx4, tx5},
+				"Node1": {tx1, tx2, tx3, tx4, tx5},
+				"Node2": {tx1, tx2, tx3, tx4, tx5},
+				"Node3": {tx1, tx2, tx3, tx4, tx5},
+				"Node4": {tx1, tx2, tx3, tx4, tx5},
+			},
+		)
+
+		set := w.BlockingSet()
+
+		assert.ElementsMatch(t, set[tx1.Hash()], []Tx{tx1})
+		assert.ElementsMatch(t, set[tx2.Hash()], []Tx{tx1, tx2})
+		assert.ElementsMatch(t, set[tx3.Hash()], []Tx{tx1, tx2, tx3})
+		assert.ElementsMatch(t, set[tx4.Hash()], []Tx{tx1, tx2, tx3, tx4})
+		assert.ElementsMatch(t, set[tx5.Hash()], []Tx{tx1, tx2, tx3, tx4, tx5})
+	})
 }
