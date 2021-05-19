bcad4238d8b9c46bb95ed6491f864485ddedf399
Gustavo Chain <gchain@pm.me>
2021-05-19 12:18:14 +0200

    docs: Add pkg documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package wendy is the reference implementation of [Wendy](https://eprint.iacr.org/2020/885), the Good Little Fairness Widget.
+// The project is still under development and it has NOT been tested in production.
+// It properly handles transactions order via a voting system.
+// It defines its own primitive types so that it remains independent from a
+// particular blockchain implementation. Primitive types are simple and should
+// satisfy most implementation.
+package wendy
